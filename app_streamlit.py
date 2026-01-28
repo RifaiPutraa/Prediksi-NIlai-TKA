6571eabd2c855234f080251be4dd9749ebabd0cc
RifaiPutraa <muhamadrifaiputra33@gmail.com>
2026-01-28 09:51:45 +0700

    Add files via upload

---
+++ app_streamlit.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# konfigurasi halaman
+st.set_page_config(
+    page_title="Prediksi Nilai TKA",
+    page_icon="📘"
+)
+
+model = joblib.load("model.joblib")
+
+st.title("📘 Prediksi Nilai TKA")
+st.markdown("Aplikasi machine learning **regression** untuk memprediksi Nilai TKA siswa")
+
+jam_belajar = st.slider("Jam Belajar per Hari", 0.0, 10.0, 4.0)
+kehadiran = st.slider("Persen Kehadiran (%)", 0.0, 100.0, 80.0)
+bimbel = st.pills("Mengikuti Bimbel?", ["ya", "tidak"], default="tidak")
+
+if st.button("Prediksi Nilai TKA", type="primary"):
+    data_baru = pd.DataFrame(
+        [[jam_belajar, kehadiran, bimbel]],
+        columns=["jam_belajar_per_hari", "persen_kehadiran", "bimbel"]
+    )
+
+    prediksi = model.predict(data_baru)[0]
+
+    st.success(f"Model memprediksi **Nilai TKA ≈ {prediksi:.0f}**")
+    st.balloons()
+
+st.divider()
+st.caption("Dibuat dengan 📘 oleh *RPL ❤*")
